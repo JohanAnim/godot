4ef14bf2bc72492569ac4aa90ea281749a5e8ae4
JohanAnim <gutierezjohanantonio@gmail.com>
2026-07-24 17:20:46 -0500

    feat(accessibility): explicitly include Alt+Down shortcut in OptionButton state description

--- scene/gui/option_button.cpp
+++ scene/gui/option_button.cpp
@@ -124,7 +124,11 @@ void OptionButton::_notification(int p_what) {
 			bool is_open = popup && popup->is_visible();
 			// 0 = none, 1 = collapsed (false), 2 = expanded (true)
 			AccessibilityServer::get_singleton()->update_set_expanded(ae, is_open ? 2 : 1);
-			AccessibilityServer::get_singleton()->update_set_state_description(ae, is_open ? atr("expanded") : atr("collapsed"));
+			String state_desc = is_open ? atr("expanded") : atr("collapsed");
+			if (!is_disabled()) {
+				state_desc += ", " + atr("Alt+Down");
+			}
+			AccessibilityServer::get_singleton()->update_set_state_description(ae, state_desc);
 
 			if (popup && popup->get_accessibility_element().is_valid()) {
 				if (is_open) {
